b56c37cf4594060b473403336fac4fc8d4526c94
IAndrexxI <andreavincent.lonardi@studenti.unipd.it>
2025-05-08 15:16:42 +0200

    TestPrint3

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -4,14 +4,46 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// Implementare test (JUnit 5)
-public class RomanPrinterTest 
-{
+public class RomanPrinterTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldPrintOne() {
+        String expected = 
+            " _____ \n" +
+            "|_   _|\n" +
+            "  | |  \n" +
+            "  | |  \n" +
+            " _| |_ \n" +
+            "|_____|\n";
+        String result = RomanPrinter.print(1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldPrintTwo() {
+        String expected = 
+            " _____   _____ \n" +
+            "|_   _| |_   _|\n" +
+            "  | |     | |  \n" +
+            "  | |     | |  \n" +
+            " _| |_   _| |_ \n" +
+            "|_____| |_____|\n";
+        String result = RomanPrinter.print(2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldPrintThree() {
+        String expected = 
+            " _____   _____   _____ \n" +
+            "|_   _| |_   _| |_   _|\n" +
+            "  | |     | |     | |  \n" +
+            "  | |     | |     | |  \n" +
+            " _| |_   _| |_   _| |_ \n" +
+            "|_____| |_____| |_____|\n";
+        String result = RomanPrinter.print(3);
+        assertEquals(expected, result);
     }
 }
