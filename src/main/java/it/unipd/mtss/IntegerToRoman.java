741441690c7d8e47895d8c1c75e51ec44e14f2dd
IAndrexxI <andreavincent.lonardi@studenti.unipd.it>
2025-05-08 14:53:01 +0200

    Implementata conversione dei primi 3 numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,9 +3,14 @@
 // [ANDREA] [LONARDI] [2113199]
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
+
 public class IntegerToRoman {
-  public static String convert(int number){
-  // Implementare funzione
-  return null;
-  }
+
+    public static String convert(int number) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            result.append("I");
+        }
+        return result.toString();
+    }
 }
