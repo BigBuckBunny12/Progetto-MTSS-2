170b3e6c872945f1f9b34075179768290228ba70
IAndrexxI <andreavincent.lonardi@studenti.unipd.it>
2025-05-08 15:12:33 +0200

    Test3

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -3,15 +3,27 @@
 // [ANDREA] [LONARDI] [2113199]
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// Implementare test (JUnit 5)
-public class IntegerToRomanTest 
-{
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegerToRomanTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldConvertOneToI() {
+        String result = IntegerToRoman.convert(1);
+        assertEquals("I", result);
+    }
+
+    @Test
+    public void shouldConvertTwoToII() {
+        String result = IntegerToRoman.convert(2);
+        assertEquals("II", result);
+    }
+
+    @Test
+    public void shouldConvertThreeToIII() {
+        String result = IntegerToRoman.convert(3);
+        assertEquals("III", result);
     }
 }
