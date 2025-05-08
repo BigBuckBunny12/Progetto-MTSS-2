04f9ba641d65f2d33f82910602334f1ccac2ae7e
IAndrexxI <andreavincent.lonardi@studenti.unipd.it>
2025-05-08 15:06:15 +0200

    Implementata stampa dei primi 3 numeri

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -3,13 +3,45 @@
 // [ANDREA] [LONARDI] [2113199]
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
+
 public class RomanPrinter {
-  public static String print(int num){
-  return printAsciiArt(IntegerToRoman.convert(num));
+
+  public static String print(int num) {
+    return printAsciiArt(IntegerToRoman.convert(num));
   }
-  private static String printAsciiArt(String romanNumber){
-  // Implementare funzione
-  return null;
+
+  private static String printAsciiArt(String romanNumber) {
+    String[] ascii = {
+        " _____ ",
+        "|_   _|",
+        "  | |  ",
+        "  | |  ",
+        " _| |_ ",
+        "|_____|"
+    };
+
+    StringBuilder[] lines = new StringBuilder[ascii.length];
+    for (int i = 0; i < lines.length; i++) {
+      lines[i] = new StringBuilder();
+    }
+
+    for (int i = 0; i < romanNumber.length(); i++) {
+      char c = romanNumber.charAt(i);
+      if (c == 'I') {
+        for (int j = 0; j < ascii.length; j++) {
+          lines[j].append(ascii[j]).append(" ");
+        }
+      }
+    }
+
+    StringBuilder finalArt = new StringBuilder();
+    for (StringBuilder line : lines) {
+      if (line.length() > 0 && line.charAt(line.length() - 1) == ' ') {
+        line.setLength(line.length() - 1);
+      }
+      finalArt.append(line).append("\n");
+    }
+
+    return finalArt.toString();
   }
 }
-
