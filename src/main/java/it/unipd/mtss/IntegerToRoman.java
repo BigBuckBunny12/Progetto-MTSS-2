c99f62b7157f5121a1fff158c6964f47ebcfa2bb
IAndrexxI <andreavincent.lonardi@studenti.unipd.it>
2025-05-08 17:19:11 +0200

    Implementata la conversione dei primi 10 numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,10 +6,24 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
 
+<<<<<<< Updated upstream
     public static String convert(int number) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < number; i++) {
             result.append("I");
+=======
+  public static String convert(int number) {
+
+    String[] romanLetters = {"X", "IX", "V", "IV", "I"};
+    int[] values = {10, 9, 5, 4, 1};
+
+    StringBuilder romanNumber = new StringBuilder();
+
+    for (int i = 0; i < values.length; i++) {
+        while (number >= values[i]) {
+            number -= values[i];
+            romanNumber.append(romanLetters[i]);
+>>>>>>> Stashed changes
         }
         return result.toString();
     }
