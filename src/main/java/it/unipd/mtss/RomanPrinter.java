e4dfb1ed21726f1eeac5f27ac8da91ea9d6f80e0
BigBuckBunny12 <gabriscaggiante1004@gmail.com>
2025-05-09 16:34:59 +0200

    Aggiornata la stampa dei primi 20 numeri

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -8,38 +8,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RomanPrinter {
-    // Supporta i numeri fino a 20
     public static String print(int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
 
-    private static final Map<Character, String[]> asciiRomanMap = new HashMap<>();
+    private static final Map<Character, String> asciiRomanMap = new HashMap<>();
 
     static {
-        asciiRomanMap.put('I', new String[]{
-            " _____ ",
-            "|_   _|",
-            "  | |  ",
-            "  | |  ",
-            " _| |_ ",
-            "|_____|"
-        });
-        asciiRomanMap.put('V', new String[]{
-            "__      __",
-            "\\ \\    / /",
-            " \\ \\  / / ",
-            "  \\ \\/ /  ",
-            "   \\  /   ",
-            "    \\/    "
-        });
-        asciiRomanMap.put('X', new String[]{
-            "__   __",
-            "\\ \\ / /",
-            " \\ V / ",
-            "  > <  ",
-            " / . \\ ",
-            "/_/ \\_\\"
-        });
+        asciiRomanMap.put('I',
+            " _____ \n" +
+            "|_   _|\n" +
+            "  | |  \n" +
+            "  | |  \n" +
+            " _| |_ \n" +
+            "|_____|\n"
+        );
+
+        asciiRomanMap.put('V',
+            "__      __\n" +
+            "\\ \\    / /\n" +
+            " \\ \\  / / \n" +
+            "  \\ \\/ /  \n" +
+            "   \\  /   \n" +
+            "    \\/    \n"
+        );
+
+        asciiRomanMap.put('X',
+            "__   __\n" +
+            "\\ \\ / /\n" +
+            " \\ V / \n" +
+            "  > <  \n" +
+            " / . \\ \n" +
+            "/_/ \\_\\\n"
+        );
     }
 
     public static String printAsciiArt(String roman) {
@@ -47,23 +48,18 @@ public class RomanPrinter {
         String[] outputLines = new String[6];
 
         for (int i = 0; i < 6; i++) {
-            StringBuilder line = new StringBuilder();
-            for (int j = 0; j < roman.length(); j++) {
-                char ch = roman.charAt(j);
-                String[] art = asciiRomanMap.getOrDefault(ch, new String[]{
-                    "       ",
-                    "       ",
-                    "       ",
-                    "   ?   ",
-                    "       ",
-                    "       "
-                });
-                line.append(art[i]);
-                if (j != roman.length() - 1) {
-                    line.append(" ");
+            outputLines[i] = "";
+        }
+
+        for (int i = 0; i < roman.length(); i++) {
+            char ch = roman.charAt(i);
+            String[] artLines = asciiRomanMap.get(ch).split("\n");
+            for (int j = 0; j < 6; j++) {
+                outputLines[j] += artLines[j];
+                if (i != roman.length() - 1) {
+                    outputLines[j] += " ";
                 }
             }
-            outputLines[i] = line.toString();
         }
 
         return String.join("\n", outputLines) + "\n";
